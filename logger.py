c847421e07a4449a27192b85e2eb2ad52bdea70b
rupam2001 <rjdasgalexy@gmail.com>
2021-12-06 15:39:01 +0530

    logger fixed

--- logger.py
+++ logger.py
@@ -1,11 +1,14 @@
 import time
 class Logger:
     def __init__(self, logFile=time.time(), change_fileInterval=None):
-        self.f = open(f"{logFile}.txt", "a")
-        self.change_fileInterval = change_fileInterval
+        # self.f = open(f"{logFile}.txt", "a")
+        # self.change_fileInterval = change_fileInterval
+        pass
 
     def log(self, text):
-        self.f.write(f"{text}  time: {time.time()}\n")
+        # self.f.write(f"{text}  time: {time.time()}\n")
+        pass
 
     def end(self):
-        self.f.close()
+        # self.f.close()
+        pass
